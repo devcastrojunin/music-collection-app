f3247c6cfcbc64027635afb1b90420865b3ee8a1
devcastrojunin <devcastrojunin@gmail.com>
2022-01-07 18:28:30 -0300

    Change route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'welcome', to: 'welcome#index'
-  root to: 'welcome#index'
+  get 'artists', to: 'artists#index'
+  root to: 'artists#index'
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
