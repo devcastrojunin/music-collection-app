b1c0fa95db7885970b7ecbd95ec187960750a438
devcastrojunin <devcastrojunin@gmail.com>
2022-01-06 22:27:08 -0300

    Role validade User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   validates :username , presence: true, uniqueness: true
   validates :full_name , presence: true
   validates :password , presence: true
-  validates :password , presence: true
+  validates :role , presence: true
 end
