c5eb7ce859d38903fb016d0788ae08b57a01aa02
devcastrojunin <devcastrojunin@gmail.com>
2022-01-07 20:21:34 -0300

    Show method created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'artists', to: 'artists#index'
+  get 'artists/:id', to: 'artists#show'
   root to: 'artists#index'
 
   get 'sign_up', to: 'registrations#new'
