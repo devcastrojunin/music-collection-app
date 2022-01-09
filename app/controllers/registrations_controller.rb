d10f28c84577a2c13d4f8136a172ffbb12511685
devcastrojunin <devcastrojunin@gmail.com>
2022-01-08 23:21:38 -0300

    Change path redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,7 +10,7 @@ class RegistrationsController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to sign_in_path
     else    
       @user.errors.full_messages
       render :new  
