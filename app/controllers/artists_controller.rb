5fdeca7af9a26287be2e60e290ac514ab3f677ac
devcastrojunin <devcastrojunin@gmail.com>
2022-01-08 23:21:22 -0300

    Action to filter artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,18 @@
 class ArtistsController < ApplicationController
   before_action :set_global_artists
   before_action :user_is_logged
+  before_action :get_albums_by_artist_id
 
   def index        
-    @artists = @artist_service.get_all    
+    @artists = @artist_service.get_all  
   end
 
   def show
-    @artists = @artist_service.get_by_id(params[:id])    
-  end
+    artist_id = params[:id]
+    @artists = @artist_service.get_by_id(artist_id)    
+    @albums = @count_albums.filter_artist_album(artist_id)
+  end  
+  
   
   private 
   
@@ -21,5 +25,10 @@ class ArtistsController < ApplicationController
       redirect_to sign_in_path
     end    
   end
+  
+  def get_albums_by_artist_id
+    @count_albums = AlbumService.new
+  end
+  
   
 end
